9d0b448504026c2a9a8cdfc366326ce19e5085d0
memoryleak47 <memoryleak47@protonmail.ch>
2020-03-26 15:49:50 +0100

    added Level & Element

---
+++ src/level/Element.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+var velocity = Vector2(0, 0)
+
+func apply_force(f):
+	print(velocity)
+	velocity += f
+
+func _physics_process(delta):
+	position += velocity
+	velocity *= 0.9
---
+++ src/level/Level.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+const REPEL_DISTANCE = 200
+
+var p = preload("res://src/level/Element.tscn")
+
+var elements = []
+
+func _add_elem(pos):
+	var element = p.instance()
+	element.position = pos
+	elements.append(element)
+	add_child(element)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for x in range(10):
+		for y in range(10):
+			_add_elem(Vector2(x*10, y*10))
+
+func _physics_process(delta):
+	for e1 in elements:
+		for e2 in elements:
+			var v = e1.position - e2.position
+			if v.length() <= REPEL_DISTANCE:
+				e1.apply_force(v / 1000)
+				e2.apply_force(-v / 1000)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
